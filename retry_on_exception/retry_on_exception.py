13877a498c89a6201e2e504131ad7080f79dddf1
jakeogh <github.com@v6y.net>
2021-01-22 15:02:51 -0700

    auto-commit

--- retry_on_exception/retry_on_exception.py
+++ retry_on_exception/retry_on_exception.py
@@ -43,6 +43,9 @@ def retry_on_exception(*,
                        in_e_args=None,
                        kwargs={},
                        args=(),
+                       kwargs_add_on_retry={},
+                       args_add_on_retry=(),
+                       kwargs_extract_from_exception=(),
                        delay=1,
                        max_delay=60,
                        retries=inf,
@@ -77,12 +80,16 @@ def retry_on_exception(*,
                 ic(delay)
                 ic(max_delay)
                 ic(retries)
+                ic(kwargs_add_on_retry)
+                ic(args_add_on_retry)
+                ic(kwargs_extract_from_exception)
                 ic(call_function_once)
                 ic(call_function_once_args)
                 ic(call_function_once_kwargs)
                 ic(delay_multiplier)
 
             raise_next = False
+            kwargs_extracted_from_exception = {}
             #ic(raise_next)
             while True:
                 if tries > (retries - 1):
@@ -95,7 +102,10 @@ def retry_on_exception(*,
                         ic(args)
                         ic(kwargs)
                     tries += 1
-                    return function(*args, **kwargs)
+                    if kwargs_extracted_from_exception:
+                        return function(*args, **kwargs, **kwargs_extracted_from_exception)
+                    else:
+                        return function(*args, **kwargs)
                 except exception as e:
                     if debug:
                         ic(e)
@@ -111,9 +121,23 @@ def retry_on_exception(*,
                                 found = True
                         if not found:
                             raise e
+
+                    if kwargs_extract_from_exception:
+                        for arg in e.args:
+                            if isinstance(arg, dict):
+                                for kw in kwargs_extract_from_exception:
+                                    if kw in arg.keys():
+                                        kwargs_extracted_from_exception[kw] = arg[kw]
+                        for kw in kwargs_extract_from_exception:
+                            if kw not in kwargs_extracted_from_exception.keys():
+                                ic(kw, 'not found in', e.args, 'exception does not match, raising:', e)
+                                raise e
+                    # by here, the exception is valid to be caught
+
+
                     if raise_next:
                         #ic(raise_next)
-                        ic('raising:', e)
+                        ic(raise_next, 'raising:', e)
                         raise e
                     ic(function)
                     if verbose:
