4082e7cbfb0d28b1e956f24293101ee5f955e404
jakeogh <github.com@v6y.net>
2021-07-14 21:13:06 -0700

    auto-commit

--- retry_on_exception/retry_on_exception.py
+++ retry_on_exception/retry_on_exception.py
@@ -98,7 +98,7 @@ def retry_on_exception(*,
                         return function(*args, **kwargs, **kwargs_extracted_from_exception)
                     else:
                         return function(*args, **kwargs)
-                except exception as e:
+                except exception: Type[Exception] as e:
                     if debug:
                         ic(e)
                     if errno:
