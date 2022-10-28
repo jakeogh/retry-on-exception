fc7bf6d34cb063ac8128e5b770a01f7f52ef8977
jakeogh <github.com@v6y.net>
2022-10-28 16:13:43 -0700

    auto-commit

--- retry_on_exception/retry_on_exception.py
+++ retry_on_exception/retry_on_exception.py
@@ -219,8 +219,9 @@ def retry_on_exception(
                                 *call_function_once_args, **call_function_once_kwargs
                             )
                             ic(call_function_once_result)
-                    ic(tries, retries)
-                    delay_timer.sleep()
+                    # ic(tries, retries)
+                    if tries < retries:
+                        delay_timer.sleep()
                 except Exception as e:
                     if verbose == inf:
                         ic(e)
