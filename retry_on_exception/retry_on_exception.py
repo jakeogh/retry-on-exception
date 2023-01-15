49068d286900d438d5ae2dd8d38b06f1cab92383
jakeogh <github.com@v6y.net>
2023-01-14 20:21:33 -0700

    auto-commit

--- retry_on_exception/retry_on_exception.py
+++ retry_on_exception/retry_on_exception.py
@@ -186,7 +186,7 @@ def retry_on_exception(
                     # by here, the exception is valid to be caught
                     _ic_state = ic.enabled
                     ic.enable()
-                    epprint("\nfound valid exception: {exception=}")
+                    epprint(f"\nfound valid exception: {exception=}")
                     ic(f"{exception}")
                     ic(
                         f"{function=}",
