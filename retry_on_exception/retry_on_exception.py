5c33d769a9cbf2c9052b248415396725b01760c6
jakeogh <github.com@v6y.net>
2024-02-22 18:54:40 -0700

    auto-commit

--- retry_on_exception/retry_on_exception.py
+++ retry_on_exception/retry_on_exception.py
@@ -144,9 +144,9 @@ def retry_on_exception(
                         type(e),
                         type(exception),
                     )
-                    if not isinstance(e, exception):
-                        if not isinstance(e, FileExistsError):
-                            icp(type(e), e, exception)
+                    if not type(e) is exception:
+                        # if not isinstance(e, FileExistsError):
+                        icp("about to raise e", type(e), e, exception)
                         raise e
                     if errno:
                         if hasattr(e, "errno"):
