47c3e710e7c78ba85be22009865631bd0515dbfe
jakeogh <github.com@v6y.net>
2024-03-19 21:09:41 -0700

    auto-commit

--- retry_on_exception/retry_on_exception.py
+++ retry_on_exception/retry_on_exception.py
@@ -1,227 +1,176 @@
 #!/usr/bin/env python3
 # -*- coding: utf8 -*-
+# tab-width:4
 
-# pylint: disable=useless-suppression             # [I0021]
-# pylint: disable=missing-docstring               # [C0111] docstrings are always outdated and wrong
-# pylint: disable=fixme                           # [W0511] todo is encouraged
-# pylint: disable=line-too-long                   # [C0301]
-# pylint: disable=too-many-instance-attributes    # [R0902]
-# pylint: disable=too-many-lines                  # [C0302] too many lines in module
-# pylint: disable=invalid-name                    # [C0103] single letter var names, name too descriptive
-# pylint: disable=too-many-return-statements      # [R0911]
-# pylint: disable=too-many-branches               # [R0912]
-# pylint: disable=too-many-statements             # [R0915]
-# pylint: disable=too-many-arguments              # [R0913]
-# pylint: disable=too-many-nested-blocks          # [R1702]
-# pylint: disable=too-many-locals                 # [R0914]
-# pylint: disable=too-few-public-methods          # [R0903]
-# pylint: disable=no-member                       # [E1101] no member for base
-# pylint: disable=attribute-defined-outside-init  # [W0201]
-# pylint: disable=too-many-boolean-expressions    # [R0916] in if statement
 from __future__ import annotations
 
-import traceback
+import logging
+import sys
 from functools import wraps  # todo
 from math import inf
+from signal import SIG_DFL
+from signal import SIGPIPE
+from signal import signal
 from typing import Type
-from typing import cast
 
-from asserttool import ic
 from asserttool import icp
 from delay_timer import DelayTimer
-from globalverbose import gvd
 
+# from typing import cast
+# import traceback
+
+logging.basicConfig(level=logging.INFO)
 # import errno as error_number
+
+# this should be earlier in the imports, but isort stops working
+signal(SIGPIPE, SIG_DFL)
+
+
+def eprint(*args, **kwargs) -> None:
+    try:
+        kwargs.pop("file")
+    except KeyError:
+        pass
+    print(*args, file=sys.stderr, **kwargs)
 
 
 def retry_on_exception(
     *,
     exception: Type[Exception],
-    errno=None,
-    in_e_args=None,
-    in_e_args_isinstance=None,
     kwargs: dict = {},
     args: tuple = (),
-    kwargs_add_on_retry={},
-    args_add_on_retry: tuple = (),
-    kwargs_extract_from_exception=(),
-    delay: float = 1.0,
-    max_delay: float = 60.0,
     retries: float = inf,
-    call_function_once=None,  # this could block, like wait_for_ping_default_gateway()
-    call_function_once_args=(),
-    call_function_once_kwargs={},
-    delay_multiplier: float = 1.3,
-    verbose: bool = False,
+    initial_delay: float = 0.0,
+    max_delay: float = 0.0,
+    delay_multiplier: float = 0.0,
+    errno: int | None = None,
+    in_e_args: str | None = None,
+    in_e_args_isinstance: type | None = None,
 ):
-    if not verbose:
-        ic.disable()
-    delay_timer = DelayTimer(
-        start=delay,
-        multiplier=delay_multiplier,
-        end=max_delay,
-    )
+    delay_timer: DelayTimer | None = None
+    if initial_delay > 0:
+        delay_timer = DelayTimer(
+            start=initial_delay,
+            multiplier=delay_multiplier,
+            end=max_delay,
+        )
 
     def retry_on_exception_decorator(function):
-        ic(f"@wraps({function=})")
-
         @wraps(function)
         def retry_on_exception_wrapper(*args, **kwargs):
-            nonlocal delay
             if not issubclass(exception, Exception):
                 raise ValueError(
                     "exception must be a subclass of Exception, not:", type(exception)
                 )
-            tries = 0
+            retry_number = 0
             if retries < 1:
                 raise ValueError("retries must be >= 1: retries:", retries)
-            assert not gvd
-            assert not ic.enabled
-            if gvd:
-                ic(
-                    exception,
-                    type(exception),
-                    kwargs,
-                    args,
-                    function,
-                    tries,
-                    retries,
-                    in_e_args,
-                    in_e_args_isinstance,
-                    errno,
-                    delay,
-                    max_delay,
-                    kwargs_add_on_retry,
-                    args_add_on_retry,
-                    kwargs_extract_from_exception,
-                    call_function_once,
-                    call_function_once_args,
-                    call_function_once_kwargs,
-                    delay_multiplier,
-                )
+            eprint(
+                f"retry_on_exception() {function=}",
+                f"{exception=}",
+                f"{type(exception)=}",
+                f"{kwargs=}",
+                f"{args=}",
+                f"{retry_number=}",
+                f"{retries=}",
+                f"{errno=}",
+                f"{in_e_args=}",
+                f"{in_e_args_isinstance=}",
+            )
 
-            raise_next = False
-            kwargs_extracted_from_exception = {}
             while True:
-                if gvd:
-                    icp(tries, retries, raise_next)
-                if tries > (retries - 1):
-                    icp(
-                        tries,
-                        ">",
-                        retries - 1,
-                        "raising next matching exception:",
-                    )
-                    raise_next = True
-                    ic(f"{raise_next=}")
+                eprint(f"while True: {retry_number=}, {retries=}")
                 try:
-                    tries += 1
-                    if kwargs_extracted_from_exception:
-                        ic("returning", function, kwargs_extracted_from_exception)
-                        return function(
-                            *args, **kwargs, **kwargs_extracted_from_exception
-                        )
-                    if gvd:
-                        icp("calling and returning:", function.__name__)
-                        icp(args, kwargs)
-                    return function(*args, **kwargs)
+                    eprint(
+                        f"while True: calling and returning: {function.__name__=}",
+                        f"{args=}",
+                        f"{kwargs=}",
+                    )
+                    _result = function(*args, **kwargs)
+                    # icp(_result)
+                    # icp(type(_result))
+                    # return function(*args, **kwargs)
+                    return _result
                 # except exception as e:  # FileNotFoundError gets caught by OSError
                 # oldbug, was not checking against decorated exception (fixed below)
                 # if exception is OSError, and e is FileNotFoundError, this will still catch, so a second check is needed
                 except exception as e:
-                    icp(exception, e)
+                    eprint(
+                        f"while True: caught {e=} with {exception=}, checking if its a keeper"
+                    )
+                    if retry_number >= retries:
+                        raise e
+                    # make sure it's the exact exception requested
                     if not type(e) is exception:
-                        # if not isinstance(e, FileExistsError):
-                        # icp("about to raise e", type(e), e, exception)
+                        eprint(
+                            f"while True: about to raise e because the exception matched, but its not the exact exception requested: {type(e)} {e=} {exception=}"
+                        )
                         raise e
                     if errno:
+                        eprint(f"while True: looking for {errno=} in {e=}")
                         if hasattr(e, "errno"):
-                            if not (cast(OSError, e).errno == errno):  # best way?
+                            if e.errno != errno:
+                                eprint(
+                                    f"while True: errno check failed {errno=}, {e.errno=}, raising e"
+                                )
                                 raise e
                         else:  # exception does not match, raise it
+                            eprint(
+                                f"while True: errno check failed {errno=} was specified, but {e=} has no errno attribute"
+                            )
                             raise e
 
                     if in_e_args:
-                        # icp(e.args)
+                        eprint(f"while True: looking for {in_e_args=} in {e.args=}")
                         found = False
                         for arg in e.args:
-                            if gvd:
-                                icp(arg)
                             try:
-                                if in_e_args in repr(arg):  # hacky, should recurse
+                                if in_e_args in repr(arg):
                                     found = True
-                            except (
-                                TypeError
-                            ):  # TODO check for: TypeError: argument of type 'MaxRetryError' is not iterable
+                            except TypeError:
                                 pass
                         if not found:
+                            eprint(
+                                f"while True: in_e_args check failed {in_e_args=} {e.args=}, raising e"
+                            )
                             raise e
 
                     if in_e_args_isinstance:
-                        if gvd:
-                            icp(e.args)
+                        eprint(
+                            f"while True: looking for {in_e_args_isinstance=} in {e.args=}"
+                        )
                         found = False
                         for arg in e.args:
                             try:
                                 if isinstance(arg, in_e_args_isinstance):
                                     found = True
-                                    if gvd:
-                                        icp("found:", arg, in_e_args_isinstance)
-                            except (
-                                TypeError
-                            ):  # TODO check for: TypeError: argument of type 'MaxRetryError' is not iterable
+                                    # icp("found:", arg, in_e_args_isinstance)
+                            # T O D O check for: TypeError: argument of type 'MaxRetryError' is not iterable
+                            except TypeError as ee:
+                                icp(ee)
                                 pass
                         if not found:
-                            icp("exception not found, raising", e)
+                            eprint(
+                                f"while True: in_e_args_isinstance check failed {in_e_args_isinstance=} {e.args=}, raising e"
+                            )
                             raise e
 
-                    if kwargs_extract_from_exception:
-                        for arg in e.args:
-                            if isinstance(arg, dict):
-                                for kw in kwargs_extract_from_exception:
-                                    if kw in arg.keys():
-                                        kwargs_extracted_from_exception[kw] = arg[kw]
-                        for kw in kwargs_extract_from_exception:
-                            if kw not in kwargs_extracted_from_exception.keys():
-                                icp(
-                                    kw,
-                                    "not found in",
-                                    e.args,
-                                    "exception does not match, raising:",
-                                    e,
-                                )
-                                raise e
+                    eprint(
+                        f"while True: caught: {e=}, and it passed all checks, not re-raising",
+                        f"{retry_number=}\n",
+                    )
 
-                    if raise_next:
-                        # ic(raise_next)
-                        icp(f"{raise_next=}", "raising:", f"{e=}")
-                        raise e
+                    retry_number += 1
+                    # for index, arg in enumerate(e.args):
+                    #    eprint(f"while True: {index=}", f"{arg=}")
+                    #    # traceback.print_exc()
 
-                    if hasattr(e, "errno"):
-                        # if cast(OSError, e).errno:  # need typing.Protocol?
-                        icp(
-                            f"{e=}", f"{e.errno=}"
-                        )  # mypy: "Exception" has no attribute "errno"  [attr-defined]
-
-                    else:
-                        icp(f"{e=}")
-
-                    for index, arg in enumerate(e.args):
-                        icp(index, arg)
-                        traceback.print_exc()
-                    if call_function_once:
-                        if tries == 1:
-                            call_function_once_result = call_function_once(
-                                *call_function_once_args, **call_function_once_kwargs
-                            )
-                            ic(call_function_once_result)
-                    # ic(tries, retries)
-                    if tries < retries:
-                        delay_timer.sleep()
                 except Exception as e:
-                    icp(e)
-                    icp(type(e))
+                    eprint(f"while True: (uncaught exception) {e=} {type(e)}")
                     raise e
+
+                if delay_timer:
+                    delay_timer.sleep()
 
         return retry_on_exception_wrapper
 
--- retry_on_exception/retry_on_exception_test.py
+++ retry_on_exception/retry_on_exception_test.py
@@ -1,74 +1,286 @@
 #!/usr/bin/env python3
+# -*- coding: utf8 -*-
 
-# pylint: disable=C0111  # docstrings are always outdated and wrong
-# pylint: disable=W0511  # todo is encouraged
-# pylint: disable=C0301  # line too long
-# pylint: disable=R0902  # too many instance attributes
-# pylint: disable=C0302  # too many lines in module
-# pylint: disable=C0103  # single letter var names, func name too descriptive
-# pylint: disable=R0911  # too many return statements
-# pylint: disable=R0912  # too many branches
-# pylint: disable=R0915  # too many statements
-# pylint: disable=R0913  # too many arguments
-# pylint: disable=R1702  # too many nested blocks
-# pylint: disable=R0914  # too many local variables
-# pylint: disable=R0903  # too few public methods
-# pylint: disable=E1101  # no member for base
-# pylint: disable=W0201  # attribute defined outside __init__
-# pylint: disable=R0916  # Too many boolean expressions in if statement
+# pylint: disable=global-statement      # [W0603]
 
 from __future__ import annotations
 
-import random
-from typing import Union
+import errno
+import os
+import time
 
 import click
-from asserttool import icp
 
 from retry_on_exception import retry_on_exception
+from retry_on_exception.retry_on_exception import eprint
+
+try_number = 0
 
 
 def raise_valueerror():
-    raise ValueError('try again')
+    raise ValueError("try again")
 
 
-#@retry_on_exception(exception=ValueError,
-#                    retries=2,)
-@retry_on_exception(exception=TypeError,
-                    retries=3,)
-def raise_multiple():
-    choice = round(random.random())
-    print(choice)
+@retry_on_exception(
+    exception=ValueError,
+    in_e_args="custom arg",
+    retries=1,
+)
+@retry_on_exception(
+    exception=ValueError,
+    in_e_args="totally different",
+    retries=1,
+)
+def raise_test_in_e_args_duplicate():
+    global try_number
+    try_number += 1
+    eprint(f"raise_test_in_e_args_duplicate() {try_number=}")
+    if try_number == 1:
+        raise ValueError("custom arg")
+    raise ValueError("totally different")
+
+
+@retry_on_exception(
+    exception=ValueError,
+    in_e_args="custom arg",
+    retries=2,
+)
+def raise_test_in_e_args():
+    global try_number
+    try_number += 1
+    eprint(f"raise_test_in_e_args() {try_number=}")
+    raise ValueError("custom arg")
+
+
+@retry_on_exception(
+    exception=OSError,
+    errno=errno.ETXTBSY,
+    retries=2,
+)
+def raise_testerrno_wrong_errno():
+    global try_number
+    try_number += 1
+    eprint(f"raise_testerrno_wrong_errno() {try_number=}")
+    raise OSError(errno.ENOSPC, os.strerror(errno.ENOSPC), "foodir")
+
+
+@retry_on_exception(
+    exception=OSError,
+    errno=errno.ENOSPC,
+    retries=2,
+)
+def raise_testerrno():
+    global try_number
+    try_number += 1
+    eprint(f"raise_testerrno() {try_number=}")
+    # cant use, will get converted to: FileNotFoundError: [Errno 2] No such file or directory: 'foodir'
+    # raise OSError(errno.ENOENT, os.strerror(errno.ENOENT), "foodir")
+    raise OSError(errno.ENOSPC, os.strerror(errno.ENOSPC), "foodir")
+
+
+@retry_on_exception(
+    exception=ValueError,
+    retries=3,
+    initial_delay=1,
+    delay_multiplier=2,
+    max_delay=8,
+)
+def raise_valueerror_slowly():
+    global try_number
+    try_number += 1
+    eprint(f"raise_valueerror_slowly() {try_number=}")
+    raise ValueError("raise_valueerror_slowly()")
+
+
+@retry_on_exception(
+    exception=OSError,
+    retries=3,
+)
+@retry_on_exception(
+    exception=FileNotFoundError,
+    retries=2,
+)
+def raise_filenotfounderror():
+    global try_number
+    try_number += 1
+    eprint(f"raise_filenotfounderror() {try_number=}")
+    raise FileNotFoundError("raise_filenotfounderror()")
+
+
+@retry_on_exception(
+    exception=ValueError,
+    retries=2,
+)
+@retry_on_exception(
+    exception=TypeError,
+    retries=3,
+)
+def raise_multiple(choice: int):
+    # choice = round(random.random())
+    global try_number
+    try_number += 1
+    eprint(f"raise_multiple() {choice=}, {try_number=}")
     if choice == 0:
-        raise ValueError
+        raise ValueError(f"raise_multiple({choice=})")
     if choice == 1:
-        raise TypeError
+        raise TypeError(f"raise_multiple({choice=})")
 
 
 @click.command()
-@click.option('--verbose', is_flag=True)
-@click.option('--ipython', is_flag=True)
-def cli(ipython: bool,
-        verbose: bool = False,
-        ):
+@click.option("--verbose", is_flag=True)
+@click.option("--ipython", is_flag=True)
+def cli(
+    ipython: bool,
+    verbose: bool = False,
+):
+    global try_number
 
-    #retry_on_exception_manual(function=raise_valueerror,
-    #                          kwargs={},
-    #                          args=(),
-    #                          retries=1,
-    #                          exception=ValueError,
-    #                          errno=None,
-    #                          verbose=verbose,)
-
-    @retry_on_exception(exception=TypeError,
-                        retries=2,)
+    @retry_on_exception(
+        exception=TypeError,
+        retries=2,
+    )
     def raise_typeerror(thing):
+        global try_number
+        try_number += 1
+        print(f"raise_typeerror(): global {try_number=}")
         thing()
 
-    raise_typeerror("1")
-    raise_multiple()
+    # test 1, simple retry test
+    eprint()
+    eprint("\ncli() Starting Test 1")
+    try_number = 0
+    try:
+        raise_typeerror("1")
+    except TypeError as e:
+        eprint(f"cli() test 1: TypeError {try_number=} need 3")
+        if try_number != 3:  # 2 retries, TypeError gets raised on the 3rd
+            raise e
+
+    # test 2, check wrapping twice ValueError
+    eprint()
+    eprint("\ncli() Starting Test 2")
+    try_number = 0
+    try:
+        raise_multiple(0)
+    except ValueError as e:
+        eprint(f"cli() test 2 ValueError: {try_number=} need 3")
+        if try_number != 3:  # 2 retries, ValueError gets raised on the 3rd
+            raise e
+
+    # test 3, check wrapping twice ValueError
+    eprint()
+    eprint("\ncli() Starting Test 3")
+    try_number = 0
+    try:
+        raise_multiple(1)
+    except TypeError as e:
+        eprint(f"cli() test 3 TypeError: {try_number=} need 4")
+        if try_number != 4:  # 3 retries, TypeError gets raised on the 4th
+            raise e
+
+    # test 4, check explicit catching of exceptions, not their parents
+    eprint()
+    eprint("\ncli() Starting Test 4")
+    try_number = 0
+    try:
+        raise_filenotfounderror()
+    except FileNotFoundError as e:
+        eprint(f"cli() test 4 FileNotFoundError: {try_number=} need 3")
+        if try_number != 3:  # 2 retries, FileNotFoundError gets raised on the 3th
+            raise e
+
+    # test 5, check delay
+    # retry 1           1s
+    # retry 2 1+(2*1) = 3s
+    # retry 3 3+(2*3) = 9s (clipped to 8)
+    # 1+3+8 = 12
+    # max is 8, so total time should be 12s
+    eprint()
+    eprint("\ncli() Starting Test 5")
+    try_number = 0
+    start_time = time.time()
+    try:
+        raise_valueerror_slowly()
+    except ValueError as e:
+        eprint(f"cli() test 5 ValueError: {try_number=} need 4")
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        eprint(f"{elapsed_time=}")
+        if try_number != 4:  # 3 retries, ValueError gets raised on the 4th
+            raise e
+        if elapsed_time < 12:
+            raise e
+        if elapsed_time > 12.1:
+            raise e
+
+    # test 6, check errno
+    eprint()
+    eprint("\ncli() Starting Test 6")
+    try_number = 0
+    try:
+        raise_testerrno()
+    except OSError as e:
+        eprint(f"cli() test 6 OSError: {try_number=} need 3")
+        if try_number != 3:  # 2 retries, OSError gets raised on the 3th
+            raise e
+
+    # check incorrect errno
+    test_number = 7
+    eprint()
+    eprint(f"\ncli() Starting Test {test_number}")
+    try_number = 0
+    try:
+        raise_testerrno_wrong_errno()
+    except OSError as e:
+        eprint(f"cli() test {test_number} OSError: {try_number=} need 1")
+        if try_number != 1:  # 0 retries, OSError gets raised on the 1th
+            raise e
+        if e.errno != errno.ENOSPC:
+            raise e
+
+    # check in_e_args
+    test_number = 8
+    eprint()
+    eprint(f"\ncli() Starting Test {test_number}")
+    try_number = 0
+    try:
+        raise_test_in_e_args()
+    except ValueError as e:
+        eprint(f"cli() test {test_number} ValueError: {try_number=} need 2")
+        if try_number != 3:  # 2 retries, ValueError gets raised on the 3th
+            raise e
+        if e.args[0] != "custom arg":
+            raise e
+
+    # check in_e_args duplicate exception, different e.args
+    test_number = 9
+    eprint()
+    eprint(f"\ncli() Starting Test {test_number}")
+    try_number = 0
+    try:
+        raise_test_in_e_args_duplicate()
+    except ValueError as e:
+        eprint(f"cli() test {test_number} ValueError: {try_number=} need 3 {e=}")
+        if try_number != 3:  # 2x1 retries, ValueError gets raised on the 3rd
+            raise e
+        # if e.args[0] != "custom arg":
+        #    raise e
+
+    ## check in_e_args_isinstance
+    # test_number = 9
+    # eprint()
+    # eprint(f"\ncli() Starting Test {test_number}")
+    # try_number = 0
+    # try:
+    #    raise_test_in_e_args_isinstance()
+    # except ValueError as e:
+    #    eprint(f"cli() test {test_number} ValueError: {try_number=} need 2")
+    #    if try_number != 3:  # 2 retries, ValueError gets raised on the 3th
+    #        raise e
+    #    if e.args[0] != "custom arg":
+    #        raise e
 
     if ipython:
-        import IPython; IPython.embed()
+        import IPython
 
-
+        IPython.embed()
