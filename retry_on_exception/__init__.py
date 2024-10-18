5da00587bbd7c3d1025fba3f2d0f78bbac609d1d
jakeogh <github.com@v6y.net>
2024-10-17 23:47:03 -0700

    auto-commit

--- retry_on_exception/__init__.py
+++ retry_on_exception/__init__.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+"""
+isort:skip_file
+"""
 
-#from .retry_on_exception import retry_on_exception, retry_on_exception_manual
-from .retry_on_exception import retry_on_exception
+
+# from .retry_on_exception import retry_on_exception, retry_on_exception_manual
+from .retry_on_exception import retry_on_exception as retry_on_exception
